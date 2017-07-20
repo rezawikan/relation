8c99f64139ab89042c8f2984d1529d895cdd8f78
rezawikan <rezza@lumonata.com>
2017-07-20 09:43:22 +0800

    =Has Many Relationship

---
+++ app/Movie.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $fillable = ['name','date_released','studio_id'];
+
+    public function studio()
+    {
+      return $this->belongsTo('App\Studio');
+    }
+}
---
+++ database/migrations/2017_07_20_012044_add_relationship_studio_and_movies.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRelationshipStudioAndMovies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->integer('studio_id')->unsigned()->change();
+            $table->foreign('studio_id')->references('id')->on('movies')
+          ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('movies', function (Blueprint $table){
+          $table->dropForeign('movies_studio_id_foreign');
+        });
+
+        Schema::table('movies', function (Blueprint $table){
+            $table->dropIndex('movies_studio_id_foreign');
+        });
+
+        Schema::table('movies', function (Blueprint $table){
+          $table->integer('studio_id')->change();
+        });
+    }
+}
